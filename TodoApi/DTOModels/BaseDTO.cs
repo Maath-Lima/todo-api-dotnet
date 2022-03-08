9785d77f44377aaf221d41a498f7829a1212d0ff
Matheus <math3226@gmail.com>
2022-03-07 21:11:46 -0300

    ModelState error validation implemented

---
+++ TodoApi/DTOModels/BaseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.DTOModels
+{
+    public abstract class BaseDTO
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
